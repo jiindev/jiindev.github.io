f1c310d883ddef7b056a1114773221a1b8f1da33
jiindev <jiindev@gmail.com>
2020-05-14 22:23:11 +0900

    Updates

---
+++ component---src-pages-about-js-55dd1f1bb8e850d23910.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"3XHS":function(e,t,n){"use strict";n.r(t),n.d(t,"default",(function(){return l}));var r=n("q1tI"),a=n.n(r);function c(e){var t=e.headerText;return a.a.createElement("h1",null,t)}function l(){return a.a.createElement("div",{style:{color:"teal"}},a.a.createElement(c,{headerText:"test"}),a.a.createElement("p",null,"test go"))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-55dd1f1bb8e850d23910.js.map
