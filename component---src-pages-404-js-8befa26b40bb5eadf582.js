bfc647a2df8de56e4365716e5318db09bf8bb009
jiindev <jiindev@gmail.com>
2020-05-30 15:32:33 +0900

    Updates

---
+++ component---src-pages-404-js-8befa26b40bb5eadf582.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return a}));var i=e("q1tI"),o=e.n(i);function a(){return o.a.createElement(c,null,"존재하지 않는 페이지입니다.")}var c=e("vOnD").default.div.withConfig({displayName:"sc-404__Wrap",componentId:"sc-1lfn62z-0"})(["height:100vh;text-align:center;margin:0 auto;display:flex;flex-direction:column;justify-content:center;font-size:24px;font-weight:700;"])}}]);
+//# sourceMappingURL=component---src-pages-404-js-8befa26b40bb5eadf582.js.map
